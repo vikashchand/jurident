3f62745f1c1c039f2350cfb241a33ad4672800ad
vikash chand <vikash.chand2020@vitstudent.ac.in>
2023-07-05 20:43:59 +0530

    fixes

--- src/Screen/Home/Home.jsx
+++ src/Screen/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import './Home.css'
-import First from '../../Components/home/First'
+
 import Second from '../../components/Home/Second'
 import Third from '../../Components/home/Third'
 import Fourth from '../../components/Home/Fourth'
 import Fifth from '../../components/Home/Fifth'
 import Sixth from '../../components/Home/Sixth'
 import Footer from '../../components/Home/Footer'
+import First from '../../components/home/First'
 
 
 
@@ -15,7 +16,6 @@ import Footer from '../../components/Home/Footer'
 const Home = () => {
   return (
     <div className='home'>
-    
 <First/>
 
 
