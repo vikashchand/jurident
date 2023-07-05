55a8e4e1947f26b39aa9f4e55d27eb6d13bb4f26
Aishwarya Daga <102050367+aishwaryadaga@users.noreply.github.com>
2023-07-05 20:38:02 +0530

    Update Accordion.jsx

--- src/components/FAQ/Accordion.jsx
+++ src/components/FAQ/Accordion.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { HiChevronDown } from "react-icons/hi";
+import { HiChevronUp } from "react-icons/hi";
 
 const Accordion = ({ title, content }) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <div className="accordion-item">
+    <div className="accordion-items">
       <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
         <div>{title}</div>
-        <div>{isActive ? " " : <HiChevronDown />}</div>
+        <div className="icons">
+          {isActive ? <HiChevronUp /> : <HiChevronDown />}
+        </div>
       </div>
       {isActive && <div className="accordion-content">{content}</div>}
     </div>
