e91f2863cb357cab5824676089ea60a3be3a3afd
SSexpl <87913198+SSexpl@users.noreply.github.com>
2023-08-12 22:15:00 +0530

    update

--- src/components/Profile/ChangePass_Page.jsx
+++ src/components/Profile/ChangePass_Page.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ChangePassBox from "./ChangePassBox";
 import ProfileBox from "./ProfileBox";
-const ChangePass_Page = () => {
+const ChangePass_Page = () => {  
+  //password change
   return (
     <>
       <header className="bg-white border-b border-black text-center text-black">
         <h1 className="text-2xl py-4">Your Header Text</h1>
       </header>
-      <div className="flex flex-wrap w-full space-x-20 justify-center">
+      <div className="flex flex-wrap w-full  justify-center lg: space-x-20">
         <div className="lg:w-3/10 sm:w-full mb-4 ">
           <ProfileBox />
         </div>
